2a87e5a49bd3aa1ad781ebcbc35ab020b9a8e2f4
Maciej Smolinski <contact@maciejsmolinski.com>
2019-09-24 15:55:11 +0200

    chore(Configuration): configure webpack to support *.purs file imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
+const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode,
+    module: {
+        rules: [
+            {
+                test: /\.purs$/,
+                loader: 'purs-loader',
+                exclude: /node_modules/,
+                query: {
+                    spago: true
+                }
+            }
+        ]
+    }
 };
